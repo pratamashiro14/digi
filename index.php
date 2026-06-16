d2663aac0dae2a70a0b16203763fb26d02a1e65f
rivky muhamad azhar <rivkymuhammad09@gmail.com>
2026-06-16 19:38:29 +0700

    deskripsi perubahan

--- index.php
+++ index.php
@@ -381,49 +381,6 @@ function render_designer_stat_card($stat) {
 		</div>
 	</section>
 
-	<div class="sec-banner bg0 p-t-80 p-b-50">
-		<div class="container">
-			<div class="row">
-				<div class="col-md-6 col-xl-4 p-b-30 m-lr-auto">
-					<div class="block1 wrap-pic-w">
-						<img src="images/banner-01.png?v=<?php echo time(); ?>" alt="IMG-BANNER">
-						<a href="product.php" class="block1-txt ab-t-l s-full flex-col-l-sb p-lr-38 p-tb-34 trans-03 respon3">
-							<div class="block1-txt-child1 flex-col-l">
-								<span class="block1-name ltext-102 trans-04 p-b-8">Ilustrasi</span>
-								<span class="block1-info stext-102 trans-04">Terpopuler</span>
-							</div>
-							<div class="block1-txt-child2 p-b-4 trans-05"><div class="block1-link stext-101 cl0 trans-09">Shop Now</div></div>
-						</a>
-					</div>
-				</div>
-                <div class="col-md-6 col-xl-4 p-b-30 m-lr-auto">
-					<div class="block1 wrap-pic-w">
-						<img src="images/banner-02.png?v=<?php echo time(); ?>" alt="IMG-BANNER">
-						<a href="product.php" class="block1-txt ab-t-l s-full flex-col-l-sb p-lr-38 p-tb-34 trans-03 respon3">
-							<div class="block1-txt-child1 flex-col-l">
-								<span class="block1-name ltext-102 trans-04 p-b-8">Tipografi</span>
-								<span class="block1-info stext-102 trans-04">Terbaru</span>
-							</div>
-							<div class="block1-txt-child2 p-b-4 trans-05"><div class="block1-link stext-101 cl0 trans-09">Shop Now</div></div>
-						</a>
-					</div>
-				</div>
-                <div class="col-md-6 col-xl-4 p-b-30 m-lr-auto">
-					<div class="block1 wrap-pic-w">
-						<img src="images/banner-03.png?v=<?php echo time(); ?>" alt="IMG-BANNER">
-						<a href="product.php" class="block1-txt ab-t-l s-full flex-col-l-sb p-lr-38 p-tb-34 trans-03 respon3">
-							<div class="block1-txt-child1 flex-col-l">
-								<span class="block1-name ltext-102 trans-04 p-b-8">UI/UX</span>
-								<span class="block1-info stext-102 trans-04">Trending</span>
-							</div>
-							<div class="block1-txt-child2 p-b-4 trans-05"><div class="block1-link stext-101 cl0 trans-09">Shop Now</div></div>
-						</a>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-
 	<section class="bg0 p-t-23 p-b-140">
 		<div class="container">
 			<div class="p-b-10"><h3 class="ltext-103 cl5">Pasar Karya</h3></div>
